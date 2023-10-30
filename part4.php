b095dfc759423f837c503c057c2f0f55bb22ee8e
JackWSL <jgb1017@gmail.com>
2023-10-29 20:42:35 -0500

    Test 2 for part 4

--- part4.php
+++ part4.php
@@ -2,10 +2,22 @@
     <body>
         <?php 
             $size = $_GET["size"];
-            for ($i = 0; $i <= $size; $i++)
+            echo '<table border="1">' . "\n";
+            echo '<tbody>' . "\n";
+            for ($i = 1; $i <= $size; $i++)
             {
-                echo "$i --> $size"
+                echo '<tr>' . "\n";
+                for ($j = 1; $j <= $size; $j++) 
+                {
+                    $val = $i * $j;
+                    echo '<td>' . "\n";
+                    echo "$val";
+                    echo '</td>' . "\n";
+                }
+                echo '</tr>' . "\n";
             }
+            echo '</tbody>' . "\n";
+            echo '</table>' . "\n";
         ?>
     </body>
 </html>
