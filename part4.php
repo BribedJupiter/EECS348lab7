3f1dae61972316121d9ccdb91aabca465e1b7ba2
JackWSL <jgb1017@gmail.com>
2023-10-29 20:57:23 -0500

    Part 4 test 5

--- part4.php
+++ part4.php
@@ -9,6 +9,21 @@
                 echo '<tr>' . "\n";
                 for ($j = 0; $j <= $size; $j++) 
                 {
+                    if ($i == 0 && $j == 0)
+                    {
+                        echo '<td></td>' . "\n";
+                        continue;
+                    }
+                    if ($i == 0)
+                    {
+                        echo '<td>' . $j . '</td>' . "\n";
+                        continue;
+                    }
+                    if ($j == 0)
+                    {
+                        echo '<td>' . $i . '</td>' . "\n";
+                        continue;
+                    }
                     $val = $i * $j;
                     echo '<td>' . "\n";
                     echo "$val";
