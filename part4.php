973327016a35a6d2ec80ce494d960a0d5553857d
JackWSL <jgb1017@gmail.com>
2023-10-29 20:23:27 -0500

    Start to part 4

---
+++ part4.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <?php 
+            $size = $_GET["size"];
+            for ($i = 0; $i <= $size; $i++)
+            {
+                echo "$i --> $size"
+            }
+        ?>
+    </body>
+</html>
